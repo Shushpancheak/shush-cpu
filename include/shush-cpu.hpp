01f333b9c4ee27647d86261cd93a50b065f93471
Maximelian Mumladze <Makcieml@gmail.com>
2019-10-20 19:54:39 +0300

    first version done

--- include/shush-cpu.hpp
+++ include/shush-cpu.hpp
@@ -9,14 +9,17 @@ namespace cpu {
 const inline std::string VERSION = "0.0.1";
 
 enum Errc {
-  ASSERTION_FAILED = -1,
-  UNKNOWN_COMMAND  = 0
+  NO_ERROR            = -2,
+  ASSERTION_FAILED    = -1,
+  UNKNOWN_COMMAND     = 0,
+  NO_FILE_GIVEN       = 1,
+  COULD_NOT_OPEN_FILE = 2,
+  FILE_TOO_LARGE      = 3
 };
 
 // In bytes, DRAM + SRAM + VRAM
-const inline size_t MEMORY_SIZE = 1048576 * .5; // == .5 Mb
+const inline size_t MEMORY_SIZE = 1048576 * 1; // == 1 Mb
 const inline size_t REGISTERS_COUNT = 32;
-const inline size_t COMMANDS_COUNT  = 256;
 
 const inline size_t CODE_SIZE   = MEMORY_SIZE / 2;
 const inline size_t VARS_POS    = CODE_SIZE;
@@ -25,9 +28,15 @@ const inline size_t VRAM_POS    = VARS_SIZE + VARS_POS;
 const inline size_t VRAM_SIZE   = MEMORY_SIZE / 5;
 
 // in int64_t's
-const inline size_t STACK_SIZE  = MEMORY_SIZE / 64; // == .5 Mb
+const inline size_t STACK_SIZE  = MEMORY_SIZE / 64; // == 1 Mb
 
 /**
+ * Loads code in memory and executes it.
+ *
+ * All operations are performed mostly on stack. The stack holds double values.
+ * Arithmetic operations are done using first popped element as first operand
+ * and the second popped as second operand.
+ *
  * Current memory map:
  * [code] [vars] [VRAM]
  * [registers]
@@ -36,15 +45,29 @@ const inline size_t STACK_SIZE  = MEMORY_SIZE / 64; // == .5 Mb
  * Memory map (future):
  * [code] [vars] [heap] [stack] [VRAM]
  * [registers]
+ *
  */
 class Cpu {
 public:
+  ~Cpu();
+
+  /**
+   * Load code in memory. Fails if the file is bad in some way.
+   */
   void LoadCode(const char* const file_name);
+  /**
+   * Does everything to start the cpu.
+   */
   void Start(int argc, char** argv);
+  /**
+   * Executes the loaded byte code.
+   */
   void Run();
 
-  inline void Push(int64_t val);
-  inline void Pop (uint8_t reg);
+  inline void Push(double val);
+  inline void Pop (uint8_t reg_id);
+
+  inline void Out();
 
   inline void Add ();
   inline void Sub ();
@@ -52,19 +75,14 @@ public:
   inline void Mod ();
   inline void Sqrt();
 
-  inline void Goto(size_t pos);
-
-  inline void In  ();
-  inline void Out ();
-
   inline void End ();
 
   const char* GetDumpMessage(int error_code);
 
 
-  char    mem[MEMORY_SIZE]     {};
-  int64_t reg[REGISTERS_COUNT] {};
-  stack::SafeStackStatic<int64_t, STACK_SIZE> stack;
+  char*   mem = new char[MEMORY_SIZE] {};
+  int64_t reg   [REGISTERS_COUNT]     {};
+  stack::SafeStackStatic<double, STACK_SIZE> stack;
 };
 
 }
--- src/shush-cpu.cpp
+++ src/shush-cpu.cpp
@@ -8,20 +8,25 @@ int main(int argc, char** argv) {
 }
 
 
+shush::cpu::Cpu::~Cpu() {
+  delete[] mem;
+}
+
+
 void shush::cpu::Cpu::LoadCode(const char* const file_name) {
   FILE* file = fopen(file_name, "rb");
 
-  if (!file) {
+  if (!file) { // TODO automatize
     std::cerr << "Could not open file \"" << file_name << "\"" << std::endl;
-    exit(1);
+    exit(Errc::COULD_NOT_OPEN_FILE);
   }
 
-  struct stat file_stat;
+  struct stat file_stat {};
   fstat(_fileno(file), &file_stat);
 
-  if (file_stat.st_size > CODE_SIZE) {
-    std::cerr << "File is too big." << std::endl;
-    exit(1);
+  if (static_cast<size_t>(file_stat.st_size) > CODE_SIZE) {
+    std::cerr << "File size is too big." << std::endl;
+    exit(Errc::FILE_TOO_LARGE);
   }
 
   fread(mem, sizeof(char), file_stat.st_size, file);
@@ -37,7 +42,7 @@ void shush::cpu::Cpu::Start(int argc, char** argv) {
 
   if (argc == 1) {
     std::cerr << "No file name given -- terminating.";
-    exit(1);
+    exit(Errc::NO_FILE_GIVEN);
   }
 
   cpu.LoadCode(argv[1]);
@@ -46,16 +51,18 @@ void shush::cpu::Cpu::Start(int argc, char** argv) {
 
 
 void shush::cpu::Cpu::Run() {
+  // Following are just temporary variables
   char cur_byte = 0;
-  int64_t arg = 0;
+  double arg = 0;
   uint8_t reg = 0;
+  size_t pos = 0;
 
   for (size_t i = 0; i < CODE_SIZE; ++i) {
     cur_byte = mem[i];
 
     switch (cur_byte) {
       case 0x00: {
-        arg = *reinterpret_cast<int64_t*>(mem + i + 1);
+        arg = *reinterpret_cast<double*>(mem + i + 1);
         i += sizeof(int64_t);
         Push(arg);
         break;
@@ -63,14 +70,144 @@ void shush::cpu::Cpu::Run() {
       case 0x01: {
         reg = *reinterpret_cast<uint8_t*>(mem + i + 1);
         i += sizeof(uint8_t);
-        Push(arg);
+        Push(reg);
+        break;
       }
 
-      case 0x02: {
-        reg = *reinterpret_cast<uint8_t*>(mem + i + 1);
-        i += sizeof(uint8_t);
+      case 0x10: {
+        Add();
+        break;
+      }
+      case 0x11: {
+        Sub();
+        break;
+      }
+      case 0x12: {
+        Div();
+        break;
+      }
+      case 0x13: {
+        Mod();
+        break;
+      }
+      case 0x14: {
+        Sqrt();
+        break;
+      }
+
+      // goto
+      case 0x20: {
+        pos = *reinterpret_cast<size_t*>(mem + i + 1);
+        i = pos; // TODO check for availability
+        break;
+      }
+
+      // in
+      case 0x30: {
+        std::cin >> arg;
         Push(arg);
+        break;
+      }
+      // out
+      case 0x31: {
+        Out();        
+        break;
+      }
+
+      case 0x40: {
+        End();
+        break;
+      }
+
+      default: {
+        std::cerr << "Unknown command" << std::endl;
+        exit(Errc::UNKNOWN_COMMAND);
       }
     }
   } 
+}
+
+
+void shush::cpu::Cpu::Push(double val) {
+  stack.Push(val);
+}
+
+
+void shush::cpu::Cpu::Pop(uint8_t reg_id) {
+  reg[reg_id] = stack.Pop();
+}
+
+
+void shush::cpu::Cpu::Out() {
+  std::cout << stack.Pop() << std::endl;
+}
+
+
+void shush::cpu::Cpu::Add() {
+  stack.Push(stack.Pop() + stack.Pop());
+}
+
+
+void shush::cpu::Cpu::Sub() {
+  stack.Push(stack.Pop() - stack.Pop());
+}
+
+
+void shush::cpu::Cpu::Div() {
+  stack.Push(stack.Pop() / stack.Pop());
+}
+
+
+void shush::cpu::Cpu::Mod() {
+  stack.Push(static_cast<int>(stack.Pop()) % 
+             static_cast<int>(stack.Pop()));
+}
+
+
+void shush::cpu::Cpu::Sqrt() {
+  stack.Push(sqrt(stack.Pop()));
+}
+
+
+void shush::cpu::Cpu::End() {
+  std::cout << "Session ended." << std::endl;
+  exit(0);
+}
+
+
+const char* shush::cpu::Cpu::GetDumpMessage(int error_code) {
+  char* msg = new char[50]; // TODO automatize
+  char err_name[50];
+  switch (error_code) {
+    case NO_ERROR : {
+      memcpy(err_name, "No error", 9);
+      break;
+    }
+    case ASSERTION_FAILED : {
+      memcpy(err_name, "Assertion failed", 17);
+      break;
+    }
+    case UNKNOWN_COMMAND : {
+      memcpy(err_name, "An unknown command was read from memory", 40);
+      break;
+    }
+    case NO_FILE_GIVEN : {
+      memcpy(err_name, "No file was given to program", 29);
+      break;
+    }
+    case COULD_NOT_OPEN_FILE : {
+      memcpy(err_name, "Could not open the file given to program", 41);
+      break;
+    }
+    case FILE_TOO_LARGE : {
+      memcpy(err_name, "File size is too big", 21);
+      break;
+    }
+    default : {
+      memcpy(err_name, "UNKNOWN ERROR", 14);
+      break;
+    }
+  }
+  sprintf(msg, "CPU dump:\nError code: %d (%s)", error_code, err_name);
+  return msg;
 }
